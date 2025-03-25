71c5c9bb61ee096be2be0e31fc7742962147f179
Adarsh <mkadarsh150@gmail.com>
2025-03-25 11:38:23 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Tailwind from './components/tailwind/Tailwind'
 import './index.css'
+import Login from './components/login/Login'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Tailwind />}></Route></Routes>
+          <Route path='/' element={<Tailwind />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   )
--- src/components/tailwind/Tailwind.jsx
+++ src/components/tailwind/Tailwind.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import "tailwindcss";
 
 
 function Tailwind() {
-  return (
-    <div className='flex'>
-        <div><h1>
-            scolor red</h1></div>
-        <div><h1>color green</h1></div>
-        <div>
-            <div><h1>color blue</h1></div>
-            <div><h1></h1></div>
+
+    let navigate = useNavigate()
+    return (
+        <div >
+            <div className='flex justify-around mt-8 items-center h-20' >
+                <div
+                    className='mr-50'><img src="src/assets/nike_.png" alt="logo of nike" width={"100px"} />
+                </div>
+                <div className='flex gap-14 text-xl'>
+                    <h2 onClick={() => navigate('/login')}>About us</h2>
+                    <h1 onClick={() => navigate('/login')}>Home</h1>
+                    <h2 onClick={() => navigate('/login')}>Location</h2>
+                    <h2 onClick={() => navigate('/login')}>Contact us</h2>
+                </div>
+                <div className='mr-30 text-xl '>
+                    <button className='pr-2 pl-2 pb-1 pt-0 bg-red-600 border-0 text-white rounded-sm'>Login</button>
+                </div>
+            </div>
+            <div className='flex gap-50 mr-15 ml-15 mt-13'>
+                <div  className=" ml-10 w-2xl h-80">
+                    <h1 className='text-base sm:text-lg md:text-2xl lg:text-8xl font-bold mb-5'>YOUR FEET<br></br> DESERVE<br></br> BETTER</h1>
+                    <button className='border-1 pr-8 pl-8 pb-2 pt-2 mr-4 rounded-md bg-red-600 text-white'>Shop Now</button>
+                    <button className='border-1 pr-8 pl-8 pb-2 pt-2 rounded-md'>Category</button>
+                </div>
+                <div className='' ><img src="src/assets/nike_shoe.avif" alt="image of shoe" width={"800px"} /></div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Tailwind
