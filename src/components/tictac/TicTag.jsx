369cb61e1ca5011a7cede17d668390d28278cf6d
Adarsh <mkadarsh150@gmail.com>
2025-03-27 12:09:33 +0530

    tailwind

--- src/components/tictac/TicTag.jsx
+++ src/components/tictac/TicTag.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function TicTag() {
-  return (
-    <div>
-        <div></div>
-    </div>
-  )
+    const [running, setRunning] = useState(false)
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        let interval;
+        if (running) {
+            interval = setInterval(() => {
+                setCount((prev) => {
+                    const newCount = prev + 1
+                    console.log("Internal running :", newCount);
+                    return newCount;
+                })
+
+            }, 1000);
+        } else {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval)
+    }, [running])
+
+    const resetTimer = () => {
+        setCount(0)
+        setRunning(false)
+    }
+    return (
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-2xl'>Timer</h1>
+
+            <div>
+                <h2>{running ? `Timer : ${count}s` : `Timer : ${count}s`}</h2>
+                <button className='text-red-500 border-1 p-1 m-1' onClick={() => setRunning(!running)}>{running ? "Stop" : "Start"}</button>
+                <button className='text-red-500 border-1 p-1 m-1' onClick={resetTimer}>Reset</button>
+            </div>
+
+        </div>
+    )
 }
 
-export default TicTag
+
+export default TicTag;
