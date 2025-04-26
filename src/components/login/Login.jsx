05c6a837ffda9357d39c74c98f02a1409d622d8d
Adarsh <mkadarsh150@gmail.com>
2025-04-26 10:39:08 +0530

    tailwind

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    let [list, setList] = useState({
+        username: '',
+        email: ''
+    })
+
+    const handleChange = (e) => {
+        let { name, value } = e.target.value
+        setList(prev => ({
+            ...prev,
+            [name]: value
+
+
+        }))
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+            <div>
+                <input type="text" name='username' onChange={handleChange} value={list.username} />
+                <input type="text" name='email' onChange={handleChange} value={list.email} />
+            </div>
+            <div>
+                <h1>Submit Button</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Login
